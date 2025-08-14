3e7287ecc7c7af2dd271baf9fa5e4f8a244c002b
matheusollima <matheus.ollima98@gmail.com>
2025-08-14 17:55:08 -0300

    conexão com api smtp para envio de email pelo formulário

--- apiPHPMail/send.php
+++ apiPHPMail/send.php
@@ -1,10 +1,42 @@
-<?php
+<?php 
+
+require __DIR__  . "/../vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
 
 
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 
+$mail = new PHPMailer(true);
 
+try {
+ $mail->isSMTP();
+ $mail->Host = $_ENV['SMTP_HOST'];
+ $mail->SMTPAuth = true;
+ $mail->Username = $_ENV['SMTP_USERNAME'];
+ $mail->Password = $_ENV['SMTP_PASSWORD'];
+ $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+ $mail->Port = $_ENV['SMTP_PORT'];
 
-
+ $mail->setFrom($_ENV['MAIL_FROM'], $_ENV['MAIL_FROM_NAME']);
+ $mail->addAddress($_ENV['MAIL_TO']);
+ $mail->isHTML(true);
+ $mail->Subject = 'Nova Mensagem do formulário';
+ $mail->Body = "<h1>Nova Mensagem</h1>
+ <p>{$_POST['Nome']}</P>
+ <p>{$_POST['Email']}</p>
+ <p>{$_POST['msgContato']}</p>
+ ";
+ $mail->CharSet = 'UTF-8';
+$mail->Encoding = 'base64';
+ $mail->send();
+ echo "Mensagem Enviada com sucesso";
+} catch (Exception $e) {
+  echo "ERRO AO ENVIAR A MENSAGEM: {$mail->ErrorInfo}";
+}
 
 
 ?>
