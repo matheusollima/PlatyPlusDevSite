37364f18f68ee2392734b8fe2b8317c7d9f13cb8
matheusollima <matheus.ollima98@hotmail.com>
2025-08-15 17:04:31 -0300

    update

---
+++ apiPHPMail/sendGrid.php
@@ -0,0 +1,11 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['msgContato'];
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$from = new SendGrid\Email(null,)
+
+
+?>
