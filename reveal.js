4e3eac4cdf57f49a5c2a919612b3048f222ee2d5
matheusollima <matheus.ollima98@gmail.com>
2025-09-03 12:19:04 -0300

    update

--- reveal.js
+++ reveal.js
@@ -44,3 +44,89 @@ revelar.reveal('.subtitulo',
     }
 )
 
+revelar.reveal('.subtitulo2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1000
+      
+    }
+)
+
+
+// Modelo de negócios 
+
+revelar.reveal('.modelo-negocio-card1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1500
+      
+    }
+)
+
+revelar.reveal('.modelo-negocio-card2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 2000
+      
+    }
+)
+
+
+
+// Cards
+
+revelar.reveal('.card1',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 1500
+      
+    }
+)
+
+revelar.reveal('.card2',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 2000
+      
+    }
+)
+
+revelar.reveal('.card3',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 2500
+      
+    }
+)
+
+revelar.reveal('.card4',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 3000
+      
+    }
+)
+revelar.reveal('.card5',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 3500
+      
+    }
+)
+
+revelar.reveal('.card6',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 4000
+      
+    }
+)
